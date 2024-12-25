bd3878dad04a40dca52485296007ba3f98b2a7fc
faris <ubaidillah.faris99@gmail.com>
2024-12-25 11:45:11 +0700

    fix: coa model (rekening)

--- app/Models/KodeRekening.php
+++ app/Models/KodeRekening.php
@@ -10,29 +10,11 @@ class KodeRekening extends Model
     use HasFactory;
 
     protected $fillable = [
-        'jenis_rekening_id',
         'nomor_rekening',
         'nama_rekening',
-        'tipe',
-        'sub_tipe',
-        'status',
+        'saldo_normal',
         'deskripsi',
     ];
-
-    
-    public function getJenisRekeningAttribute()
-    {
-        $idJenis = $this->kode_rekening['id_jenis'];
-        return JenisRekening::where('id_jenis', $idJenis)->first();
-    }
-
-    public function jenis_rekening(){
-        return $this->belongsTo(JenisRekening::class,'jenis_rekening_id','id');
-    }
-
-    public function rekening_plotting(){
-        return $this->hasOne(RekeningPlotting::class,'kode_rekening_id','id');
-    }
 
     public function bukuBesars()
     {
--- resources/js/Pages/Neraca/Index.vue
+++ resources/js/Pages/Neraca/Index.vue
@@ -1,4 +1,4 @@
-<template>
+`<template>
     <Head title="Neraca" />
 
     <AuthenticatedLayout>
@@ -16,8 +16,8 @@
                     </div>
 
                     <!-- Table untuk menampilkan rekap neraca -->
-                    <Table 
-                    :headers="headers" 
+                    <Table
+                    :headers="headers"
                     :data="tableData"
                     :startRow="startRow"
                     :links="links"
@@ -41,10 +41,10 @@
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="#20744a" fill-rule="evenodd" d="M28.781 4.405h-10.13V2.018L2 4.588v22.527l16.651 2.868v-3.538h10.13A1.16 1.16 0 0 0 30 25.349V5.5a1.16 1.16 0 0 0-1.219-1.095m.16 21.126H18.617l-.017-1.889h2.487v-2.2h-2.506l-.012-1.3h2.518v-2.2H18.55l-.012-1.3h2.549v-2.2H18.53v-1.3h2.557v-2.2H18.53v-1.3h2.557v-2.2H18.53v-2h10.411Z"/><path fill="#20744a" d="M22.487 7.439h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323zm0 3.501h4.323v2.2h-4.323z"/><path fill="#fff" fill-rule="evenodd" d="m6.347 10.673l2.146-.123l1.349 3.709l1.594-3.862l2.146-.123l-2.606 5.266l2.606 5.279l-2.269-.153l-1.532-4.024l-1.533 3.871l-2.085-.184l2.422-4.663z"/></svg>
                             <span>Download</span>
                         </a>
-                        <TahunFilter 
+                        <TahunFilter
                         @on-change="(value)=>{selectedYear = value}"/>
                     </template>
-                      
+
                     </Table>
                 </div>
             </template>
@@ -114,11 +114,11 @@ export default {
         getData() {
             console.log(this.length)
             // Mengambil data rekap neraca berdasarkan tahun yang dipilih
-            axios.get(route('neraca.show', { 
+            axios.get(route('neraca.show', {
                 page: this.page,
                 length : this.length,
                 search: this.searchQuery,
-                tahun: this.selectedYear 
+                tahun: this.selectedYear
             })).then(response => {
                 this.responseData = response.data;
                 this.tableData = response.data.data.map(item => ({
