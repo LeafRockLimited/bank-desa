df494a39c6084864c75470179bc3cd0aa3ad5760
faris <ubaidillah.faris99@gmail.com>
2025-02-09 10:33:59 +0700

    laba rugi neraca bkd

--- resources/js/Components/InputCurrency.vue
+++ resources/js/Components/InputCurrency.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {computed, ref, watch, defineEmits} from 'vue';
+import {computed, ref, watch} from 'vue';
 
 const props = defineProps({
     value: {
--- resources/js/Pages/LabaRugi/Index.vue
+++ resources/js/Pages/LabaRugi/Index.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
     <Head title="Laba Rugi"/>
 
     <AuthenticatedLayout>
